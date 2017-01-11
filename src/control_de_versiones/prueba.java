6b56899a697b7891446143a1d36e49697689fbac
admin1 <admin1@B076031>
2017-01-11 10:20:04 +0100

    dasda

--- src/control_de_versiones/prueba.java
+++ src/control_de_versiones/prueba.java
@@ -3,6 +3,7 @@ package control_de_versiones;
 public class prueba{
 	
 	public static void main() {
-		System.out.println("sadasdasda");
+		System.out.println("sadasasdasdasdasdasddasda");
+		System.out.println("sadasasdasdasdasdasddasda");
 	}
 }
